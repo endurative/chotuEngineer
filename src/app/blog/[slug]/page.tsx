33835ce9788ff7d51ea8d25a4bfb1070f2e37620
Harshit Singhal <harshit.singhal@coderower.com>
2025-03-26 15:23:23 +0530

    try to fix slug issue

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,19 @@
-import { notFound } from 'next/navigation';
-import { use } from 'react';
+import { notFound } from "next/navigation";
+import { use } from "react";
 
 interface BlogPost {
   title: string;
   content: string;
 }
 
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
 export async function generateStaticParams() {
-  const res = await fetch('http://localhost:3000/api/blogs');
+  const res = await fetch("http://localhost:3000/api/blogs");
   const posts = await res.json();
 
   return posts.map((post: any) => ({
@@ -15,9 +21,9 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPost({ params }: { params: Promise<any> }) {
-  const { slug } = use(params);
-  
+export default async function BlogPost({ params }: Props) {
+  const { slug } = params;
+
   const res = await fetch(`http://localhost:3000/api/blogs/${slug}`, {
     next: { revalidate: 600 }, // Revalidate the page every 10 minutes
   });
@@ -31,9 +37,9 @@ export default async function BlogPost({ params }: { params: Promise<any> }) {
     <div className="max-w-4xl mx-auto px-4 py-8">
       <article className="prose prose-lg dark:prose-invert max-w-none">
         <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
-        <div 
+        <div
           className="blog-content"
-          dangerouslySetInnerHTML={{ __html: post.content }} 
+          dangerouslySetInnerHTML={{ __html: post.content }}
         />
       </article>
     </div>
